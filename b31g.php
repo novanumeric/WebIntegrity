8cfb7717a399b8a8cff856dfb96cdc4cdac0a828
Jeff Brubaker <brubaker.jeff@novanumeric.com>
2013-02-04 12:38:12 -0500

    Update b31g.php
    cleaned up javascript

--- b31g.php
+++ b31g.php
@@ -32,11 +32,10 @@
 <? if(file_exists("../inc/header.php")) { require "../inc/header.php"; }?>
 <? require "includes/headerB31G.php"; ?>
 <? require "includes/pipeSchedules.php"; ?>
+
 <script src="includes/sharedJavascript.js" type="text/javascript"></script>
-
-
 <div style="position:absolute;background-color:white;left:25px;top:100px;">
-<span><b  style="color:blue">Level 1 B31G Calculations:<br>The following procedures calculate the maximum pressure for damaged piping per ASME B31G-2009.</b></span><br>
+<span><b  style="color:blue">B31G Calculations:<br>The following procedures calculate the maximum pressure for damaged piping per ASME B31G-2009.</b></span><br>
 <span id="depthMessage" style="color:red"></span>
 <table >
 
@@ -54,31 +53,23 @@
 <tr id="PitDepthsLevel1"><td><span class="WithHints" title="d = Maximum Depth of Metal Loss">Depth (d)</span></td><td><input type="text" class="Calculate" id="CalcsDepth"  size=6 value="4"></td><td> <span class="LengthUnits">(in)</span> </td></tr>
 <tr><td><span class="WithHints" title="L = length of metal loss">Length (L)</span></td><td><input type="text" class="Calculate" id="CalcsLength"  size=6 value="210"></td><td> <span class="LengthUnits">(in)</td></tr>
 <tr><td><span class="WithHints" title="Sflow =<br>
-										   (b.1) min(1.1xSMYS,SMTS) for plain Carbon Steel less than 120°C (120°F)<br>
+										   (b.1) min(1.1xSMYS,SMTS) for plain Carbon Steel less than 120ï¿½C (120ï¿½F)<br>
 										   (b.2) = SMYS + 69 MPa (10 ksi) for plain carbon and low alloy steel having<br>
 										         SMYS not in excess of 70 ksi (483 MPa)<br> 
-											 and operating at temperatures below 120°C (250°F)<br>
+											 and operating at temperatures below 120ï¿½C (250ï¿½F)<br>
 											(b.3) = (S<sub>YT</sub>+S<sub>UT</sub>/2) ASME B31G 2009 1.7(b)">S<sub>flow</sub></span></td><td><input type="text" id="Sflow"  size=6 value="554"></td><td> <span class="StressUnits">(MPa)</span></td></tr>
 </table></td>
 <td>
 <table id="PitDepthsLevel2">
-  <tr><td>Increment <span class="LengthUnits"></span></td><td>Pit Depth <span class="LengthUnits"></span></td></tr>
+  <tr><td>Increment<br>S <span class="LengthUnits"></span></td><td>Pit Depth<br>d <span class="LengthUnits"></span></td></tr>
   <tr><td><textarea id="CTPInputMLengths" rows=20 cols=5></textarea></td><td><textarea id="CTPInputM" rows=20 cols=5></textarea></td></tr>
 </table>
 </td>
 <td valign="top">
-
 <img id="work"  src="">
 </td>
 </tr>
-
 </table>
-
-	
-
-
-<br>
-
 
 </div>
 <!-- <span style="position:absolute;left:50px;top:550px">Numbers matched (*except case 2) for <a target="_new" href="http://www.asme-ipti.org/attachments/files/646/Track%204%20Ben%20Verhagen.pdf">"VALIDATON OF THE ASME B31G AND RSTRENG METHODOLOGIES FOR THE DETERMINATION OF THE BURST PRESSURE OF CORRODED PIPES IN API 5L X70 / EN 10208-2 L485"</a></span>-->
@@ -163,25 +154,20 @@ $(function() {
 		});
 	});
 	displayNPS();
+	
 	$('.WithHints').powerTip({placement: 'ne',	mouseOnToPopup: true});
-
 	$("#selectNPS").change(displayNPS);
 	$("#selectSchedules").change(displayGeometry);
 	$('#selectUnits').change(function test() {displayNPS(); calculate();});
-	$('#selectMethod').change(calculate);
-	$('#selectUnits').val(getParameterByName('selectUnits'),'English');
-	
+	$('#selectUnits').val(getParameterByName('selectUnits','English'));	
 	$('#CalcsDiameter').val(getParameterByName('CalcsDiameter',60));
 	$('#CalcsDiameter').val(getParameterByName('CalcsUniformThickness',5));
 	$('.Calculate').change(calculate);
-
 	
 	$('#calc').click(calculate);
 	setupLinks();
 	$(document).ready(calculate);
 });
-
-
 
 $("#ClosePipeScheduleLookup").click(function () {
 	$("#PipeScheduleLookup").hide(); 
@@ -193,14 +179,11 @@ $("#btnSelect").click(function () {
 	$("#PipeScheduleLookup").hide(); 
 });
 
-
 $("#ShowPipeScheduleLookup").click(function () {
 	displayNPS();
 	$("#PipeScheduleLookup").show();
 	$("#PipeScheduleLookup").draggable();
 });
-
-
 
 </script>
 </HTML>
