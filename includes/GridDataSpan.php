17820422552ab2d066da2a3b6e647cd448366e15
Jeff Brubaker <brubaker.jeff@novanumeric.com>
2013-02-04 12:40:15 -0500

    Update includes/GridDataSpan.php
    cleaned up spacing

--- includes/GridDataSpan.php
+++ includes/GridDataSpan.php
@@ -1,20 +1,20 @@
-
 <table id="GridDataSpan" class="ui-widget-content" style="border-style:solid;border-color:black;border-width:1px;">
 <tr><td>
 <label for="GridInputData">Thickness Grid <span class="LengthUnits"></span>:</label><br>
-<table ><tr><td><table> 
-<tr><td></td><td>C1 C2 C3 C4 ....</td></tr>
-<tr>
-<td style="vertical-align:top">M1<br>M2<br>M3<br>M4<br>..<br>..<br>..</td>
-<td>
-<textarea id="GridInputData" rows=12 cols=50>
-</textarea></td>
-</tr>
-</table></td>
-<td style="vertical-align:text-top;">
-<span id="GridTable" style="font-size:10px;">
-<table border=0 cellspacing=2 id="GridData"></table> 
-</span>
-</td></tr></table>
+  <table ><tr><td>
+    <table> 
+     <tr><td></td><td>C1 C2 C3 C4 ....</td></tr>
+     <tr>
+       <td style="vertical-align:top">M1<br>M2<br>M3<br>M4<br>..<br>..<br>..</td>
+       <td>
+            <textarea id="GridInputData" rows=12 cols=50></textarea>
+      </td>
+     </tr>
+    </table>
+   </td><td style="vertical-align:text-top;">
+      <span id="GridTable" style="font-size:10px;">
+         <table border=0 cellspacing=2 id="GridData"></table> 
+      </span>
+    </td></tr></table>
 </td></tr>
 </table>
