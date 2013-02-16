6a23204100e9cfa7c0e517a44f618455225f78d0
Novanumeric <brubaker.jeff@novanumeric.com>
2013-02-15 21:21:28 -0500

    Updates to Brittle

---
+++ includes/BrittleResults.php
@@ -0,0 +1,5 @@
+<div id="BrittleResults" class="ui-widget-content" style="position:absolute;border-style:solid;left:510px;top:80px;border-color:black;border-width:1px;"> 
+
+<div id="GovThickness" style="height:250px; width:400px;"></div>
+<div id="TempReduction" style="height:250px; width:400px;"></div>
+</div>
